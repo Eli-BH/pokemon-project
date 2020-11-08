1185860ccff9efbecf1d4ff47fb2c24c54b92e87
Eli-b-good <eli.i.henderson95@gmail.com>
2020-11-08 18:01:19 -0500

    heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node pokemon-backend/server.js
--- pokemon-backend/server.js
+++ pokemon-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
@@ -13,6 +14,22 @@ connectDB();
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
+
+var __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/pokemon-client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "pokemon-client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running...");
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
